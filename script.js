a553c478fb842720b03e81130e94325c548c5e4f
devmahin <mahinhowlader123@gmail.com>
2024-01-26 20:58:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const salary = 35000;
+const BCS = true;
+let height = 64;
+
+
+if(20000 < salary){
+    console.log("Kobul")
+}else{
+    console.log("Tomar kono jogota nai")
+}
+
+
+
+if( 21000 <= salary || height > 70){
+    console.log("Parfect gorgious")
+}else{
+    console.log("Not parfect")
+}
+
+
+if(salary >= 30000  && height <= 78){
+    console.log("Parfect gorgious")
+}else{
+    console.log("Not parfect")
+}
+
